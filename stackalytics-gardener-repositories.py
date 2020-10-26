fbf83d5f553375f5912d8b1334bb33e7cc40f00b
Christian Berendt <berendt@23technologies.cloud>
2020-10-26 21:34:16 +0100

    Script to generate Stackalytics configuration for Gardener
    Signed-off-by: Christian Berendt <berendt@23technologies.cloud>

---
+++ stackalytics-gardener-repositories.py
@@ -0,0 +1,31 @@
+import github
+import json
+
+config = []
+modules = []
+
+gh = github.Github()
+ 
+for repository in gh.get_organization("gardener").get_repos(sort='full_name'):
+
+    if repository.name == 'gardener':
+        data = {
+            'module': repository.name,
+            'organization': 'gardener',
+            'url': repository.clone_url
+        }
+        modules.append(repository.name)
+    else:
+        data = {
+            'module': "%s@gardener" % repository.name,
+            'organization': 'gardener',
+            'url': repository.clone_url
+        }
+        modules.append("%s@gardener" % repository.name)
+
+    config.append(data)
+
+print(json.dumps(config, indent=4))
+print
+print
+print(json.dumps(modules, indent=4))
